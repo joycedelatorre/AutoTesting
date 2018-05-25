3427bf11aa7374a3ba2e20b5e2ff31d4b4d7adba
joyce <joycedelatorre87@gmail.com>
2018-05-25 13:28:19 -0500

    add js file with webdriver boiler plate

---
+++ selenium-test/google_test.js
@@ -0,0 +1,29 @@
+var webdriver = require('selenium-webdriver'),
+    By = webdriver.By,
+    until = webdriver.until;
+
+var driver = new webdriver.Builder()
+    .forBrowser('firefox')
+    .build();
+
+driver.get('https://www.google.com');
+
+driver.findElement(By.name('q')).sendKeys('little mamas filipino food austin');
+
+//driver.sleep(1000).then(function() {
+driver.findElement(By.name('q')).sendKeys(webdriver.Key.TAB);
+// });
+
+ driver.findElement(By.name('btnK')).click();
+
+// driver.sleep(2000).then(function() {
+//   driver.getTitle().then(function(title) {
+//     if(title === 'webdriver - Google Search') {
+//       console.log('Test passed');
+//     } else {
+//       console.log('Test failed');
+//     }
+//   });
+// });
+
+// driver.quit();
