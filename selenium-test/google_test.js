6dea7224f14fdaa02c14dc284c050c8b724d14a6
joyce <joycedelatorre87@gmail.com>
2018-05-25 23:08:21 -0500

    add extend and PASS: upto about page NEXT: getting the text address, phone, hours

--- selenium-test/google_test.js
+++ selenium-test/google_test.js
@@ -1,3 +1,4 @@
+var extend = require('selenium-extend');
 var webdriver = require('selenium-webdriver'),
     By = webdriver.By,
     until = webdriver.until;
@@ -6,20 +7,50 @@ var driver = new webdriver.Builder()
     .forBrowser('firefox')
     .build();
 
-driver.get('https://www.google.com');
+extend.addExtend(driver);
+
+driver.get('https://www.google.com/');
 
 driver.findElement(By.name('q')).sendKeys('little mamas filipino food austin');
 
-//driver.sleep(1000).then(function() {
-driver.findElement(By.name('q')).sendKeys(webdriver.Key.TAB);
+
+driver.sleep(5000).then(function() {
+  driver.findElement(By.name('q')).sendKeys(webdriver.Key.TAB);
+  // driver.extend.doubleClick('#btnK');
+});
+
+// driver.sleep(5000).then(function(){
+
+  driver.findElement(By.name('btnK')).click();
 // });
 
-driver.findElement(By.name('btnK')).click();
-
 driver.sleep(5000).then(function(){
-  driver.findElement(By.linkText('Website')).click();
-  
+  driver.findElement(By.linkText('Website')).click(); 
+  driver.sleep(5000).then(function(){
+      driver.findElement(By.linkText('About')).click();
+
+
+      driver.sleep(5000).then(function(){
+      //   driver.extend.getText('_2iem');
+          var address = driver.findElement(By.class("span[class='_2iem']"));
+          for(var i = 0; i < address.length; i ++){
+            console.log(driver.getText(address[i]));
+            
+          }
+      });
+  });
 });
+
+
+// driver.findElement(By.class("span[class='_2iem']")).getText().then(function(text){
+//   console.log(text);
+// });
+
+// driver.extend.getText("span[class='_2iem']").then(function(text){
+//   console.log(text);
+// });
+
+
 // driver.sleep(2000).then(function() {
 //   driver.getTitle().then(function(title) {
 //     if(title === 'webdriver - Google Search') {
